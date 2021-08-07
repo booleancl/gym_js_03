67a9a2ea278833219708d781f49e27e8761c2f2b
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2021-08-07 09:55:08 -0400

    ajax exercise

--- promises/shopping-list.test.js
+++
@@ -1,9 +0,0 @@
-const shoppingList = require('./shopping-list')
-
-
-describe('ShoppingList function', () => {
-  it('Fetch the lists based on User Name', async () => {
-    const list = await shoppingList('Winston')
-    expect(list).toBe([])
-  })
-})
